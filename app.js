fa0707ba6277dd2ef7e411c56dff958263015756
Danny Li <dannyli0109@gmail.com>
2017-07-31 17:17:43 +1000

    melbourne weather

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 var request = require('request');
+var fs = require("fs")
 
+
+request('http://api.openweathermap.org/data/2.5/weather?q=melbourne,Australia&units=metric&apikey=73159aeecb90012fa048a38a5e33b991', function(err, res, body) {
+    var json = JSON.parse(body);
+    console.log("City: " + json.name + "," + json.sys.country);
+    console.log(json.weather[0].description)
+    console.log("Current temperature: " + json.main.temp + " C")
+    console.log("Lowest temperature: " + json.main.temp_min + " C");
+    console.log("Heightest temperature: " + json.main.temp_max + " C");
+    console.log("Humidity: " + json.main.humidity + "%")
+});
